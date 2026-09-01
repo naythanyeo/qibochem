f3a1bce53510d79ea6ddce10a6f93d445f6ee829
Guang Liang <guangliangyeo@gmail.com>
2026-09-01 10:20:00 +0800

    removed qse test

--- run-test/tups-test.py
+++ run-test/tups-test.py
@@ -28,7 +28,6 @@ sv_protocol = StateVectorProtocol()
 num_active_e = 6
 num_active_o = 6
 molecule_name = 'h6'
-n_layers = 1
 input_dir = "./data/"
 mol = load_molecule(
             SCRIPT_DIR / 'data' / '28_mols' / f"{molecule_name}.xyz",
@@ -37,21 +36,15 @@ mol = load_molecule(
             orbitals='canonical'
         )
 
-ref_bitstr = '110011001100'
+ref_bitstr = '110011001100' # initial bit string to specify occupancy of orbitals
 # perm = [0,4,2,3,1,5]
-perm = [0,5,1,4,2,3]
-perm = None
+perm = [0,5,1,4,2,3] # permutation to rearrange orbitals when perfect_pair==True, optional
 # perm = [0,3,1,4,2,5]
-# initial_guess = np.fromstring(array_text, sep=' ')
 for i in range(1):
-    tups = Ansatz_tUPS(mol=mol, layers=1, oo_layers=0, use_random_angles=False, use_mp2_guess=False, 
+    tups = Ansatz_tUPS(mol=mol, layers=2, oo_layers=0, use_random_angles=False, use_mp2_guess=False, 
                     use_projection=True, use_mat_mul=True, perfect_pair=True, ref_bitstring=ref_bitstr, mo_perm=perm
                     )
 
     tups.run_vqe(protocol=sv_protocol, fast=True, callback=callback, method='L-BFGS-B', fast_mat_mul=True,)
-    # tups.get_spat_1rdm()
-    # tups.get_spat_2rdm()
-    # print(tups.t_rdm)
     print(tups.param_names)
     print(tups.energy)
-# print(tups.final_circuit)
